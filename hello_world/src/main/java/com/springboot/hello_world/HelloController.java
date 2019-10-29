a086e38166424ce245670c5d688fe332d2367b79
zhoubing <1289659617@qq.com>
2019-10-29 08:48:42 +0800

    配置文件

---
+++ hello_world/src/main/java/com/springboot/hello_world/HelloController.java
@@ -0,0 +1,13 @@
+package com.springboot.hello_world;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "hello springboot";
+    }
+}
