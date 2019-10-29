56ad5738c886905571146545865a81503eaab192
zhoubing <1289659617@qq.com>
2019-10-29 08:30:00 +0800

    Initial commit

---
+++ src/test/java/com/bing/springmboot/SpringmbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bing.springmboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
